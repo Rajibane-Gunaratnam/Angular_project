a2481f568607b5d5ab2d620122cd73c14a614f2f
Rajibane <rajibane.gunaratnam@gmail.com>
2023-11-12 00:57:34 +0100

    Ajout student.mock.ts

---
+++ src/mocks/student.mock.ts
@@ -0,0 +1,15 @@
+import { Student } from '../models/student';
+
+export const STUDENT_MOCKED: Student[] = [
+    {
+        id: 1,
+        nom:  "Delmotte",
+        prenom: "Jean-Yves"
+    },
+    {
+        id: 2,
+        nom: "Pourtier",
+        prenom: "Rémi"
+    }
+
+];
